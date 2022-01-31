15eb8cd7d81f6458128b8649766f0be97a5c7c32
Nicola Venturi <n.venturi@nextworks.it>
2022-01-31 12:10:02 +0100

    Added aggregator

--- nssmf-service/src/main/java/it/nextworks/nfvmano/nssmf/service/NssmfLcmService.java
+++ nssmf-service/src/main/java/it/nextworks/nfvmano/nssmf/service/NssmfLcmService.java
@@ -27,7 +27,6 @@ import it.nextworks.nfvmano.nssmf.service.messages.provisioning.TerminateNssiReq
 import it.nextworks.nfvmano.nssmf.service.nbi.NsmfNotifier;
 import it.nextworks.nfvmano.nssmf.record.RecordServiceFactory;
 import it.nextworks.nfvmano.nssmf.service.nssmanagement.NssLcmEventHandler;
-import it.nextworks.nfvmano.oran.recordservice.service.OranRecordService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
